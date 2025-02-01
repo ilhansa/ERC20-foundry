48a887a6de16b19392f6cf574f3e342bb4e2c29a
Yulia Nadzifah <ilhansalihilsa@gmail.com>
2025-02-01 16:49:19 +0700

    Manual Token

---
+++ src/ManualToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+contract ManualToken {
+    function name() public pure returns (string memory) {
+        return "Manual Token";
+    }
+}
